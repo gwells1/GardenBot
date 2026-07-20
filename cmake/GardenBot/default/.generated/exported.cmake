60288219a37a821a812db108f031ef906fe3df65
Gregory Wells <gregwells1530@gmail.com>
2026-07-19 21:14:09 -0700

    test commit, updated build files

--- cmake/GardenBot/default/.generated/exported.cmake
+++ cmake/GardenBot/default/.generated/exported.cmake
@@ -1,13 +1,13 @@
-set(DEPENDENT_MP_BIN2HEXGardenBot_default_1S4E_cJp "c:/Program Files/Microchip/xc32/v5.00/bin/xc32-bin2hex.exe")
-set(DEPENDENT_DEPENDENT_TARGET_ELFGardenBot_default_1S4E_cJp ${CMAKE_CURRENT_LIST_DIR}/../../../../out/GardenBot/default.elf)
-set(DEPENDENT_TARGET_DIRGardenBot_default_1S4E_cJp ${CMAKE_CURRENT_LIST_DIR}/../../../../out/GardenBot)
-set(DEPENDENT_BYPRODUCTSGardenBot_default_1S4E_cJp ${DEPENDENT_TARGET_DIRGardenBot_default_1S4E_cJp}/${sourceFileNameGardenBot_default_1S4E_cJp}.c)
+set(DEPENDENT_MP_BIN2HEXGardenBot_default_b6TNGsHU "c:/Program Files/Microchip/xc32/v5.00/bin/xc32-bin2hex.exe")
+set(DEPENDENT_DEPENDENT_TARGET_ELFGardenBot_default_b6TNGsHU ${CMAKE_CURRENT_LIST_DIR}/../../../../out/GardenBot/default.elf)
+set(DEPENDENT_TARGET_DIRGardenBot_default_b6TNGsHU ${CMAKE_CURRENT_LIST_DIR}/../../../../out/GardenBot)
+set(DEPENDENT_BYPRODUCTSGardenBot_default_b6TNGsHU ${DEPENDENT_TARGET_DIRGardenBot_default_b6TNGsHU}/${sourceFileNameGardenBot_default_b6TNGsHU}.c)
 add_custom_command(
-    OUTPUT ${DEPENDENT_TARGET_DIRGardenBot_default_1S4E_cJp}/${sourceFileNameGardenBot_default_1S4E_cJp}.c
-    COMMAND ${DEPENDENT_MP_BIN2HEXGardenBot_default_1S4E_cJp} --image ${DEPENDENT_DEPENDENT_TARGET_ELFGardenBot_default_1S4E_cJp} --image-generated-c ${sourceFileNameGardenBot_default_1S4E_cJp}.c --image-generated-h ${sourceFileNameGardenBot_default_1S4E_cJp}.h --image-copy-mode ${modeGardenBot_default_1S4E_cJp} --image-offset ${addressGardenBot_default_1S4E_cJp} 
-    WORKING_DIRECTORY ${DEPENDENT_TARGET_DIRGardenBot_default_1S4E_cJp}
-    DEPENDS ${DEPENDENT_DEPENDENT_TARGET_ELFGardenBot_default_1S4E_cJp})
+    OUTPUT ${DEPENDENT_TARGET_DIRGardenBot_default_b6TNGsHU}/${sourceFileNameGardenBot_default_b6TNGsHU}.c
+    COMMAND ${DEPENDENT_MP_BIN2HEXGardenBot_default_b6TNGsHU} --image ${DEPENDENT_DEPENDENT_TARGET_ELFGardenBot_default_b6TNGsHU} --image-generated-c ${sourceFileNameGardenBot_default_b6TNGsHU}.c --image-generated-h ${sourceFileNameGardenBot_default_b6TNGsHU}.h --image-copy-mode ${modeGardenBot_default_b6TNGsHU} --image-offset ${addressGardenBot_default_b6TNGsHU} 
+    WORKING_DIRECTORY ${DEPENDENT_TARGET_DIRGardenBot_default_b6TNGsHU}
+    DEPENDS ${DEPENDENT_DEPENDENT_TARGET_ELFGardenBot_default_b6TNGsHU})
 add_custom_target(
-    dependent_produced_source_artifactGardenBot_default_1S4E_cJp 
-    DEPENDS ${DEPENDENT_TARGET_DIRGardenBot_default_1S4E_cJp}/${sourceFileNameGardenBot_default_1S4E_cJp}.c
+    dependent_produced_source_artifactGardenBot_default_b6TNGsHU 
+    DEPENDS ${DEPENDENT_TARGET_DIRGardenBot_default_b6TNGsHU}/${sourceFileNameGardenBot_default_b6TNGsHU}.c
     )
